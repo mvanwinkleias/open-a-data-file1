3ec5c6bd58f26a0a4d7016c309d419d78ddc1e6a
Martin VanWinkle III <mvanwinkle@ias.edu>
2020-12-30 08:14:40 -0500

    initial checkin

---
+++ src/main.c
@@ -0,0 +1,29 @@
+#include <config.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) {
+	puts ("Hello world from " PACKAGE_STRING);
+
+	FILE *file_pointer; 
+	char c;
+	char * file_name = DATADIR "/blah.txt";
+	puts ("File name: ");
+	puts(file_name);
+	file_pointer = fopen(file_name, "r"); 
+	if (file_pointer == NULL) 
+	{ 
+		printf("Cannot open file \n"); 
+		exit(1); 
+	} 
+  
+	c = fgetc(file_pointer); 
+	while (c != EOF) 
+	{ 
+		printf ("%c", c); 
+		c = fgetc(file_pointer); 
+	} 
+  
+	fclose(file_pointer); 
+	return 0; 
+}
